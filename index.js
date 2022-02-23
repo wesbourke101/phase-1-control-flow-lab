a71aa0075de6c1771b002ae21b3d2f0f64740c5e
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-22 21:54:01 -0500

    commit tree 720db61d69e2e7fd4cc8a2acf4e83932afc62e97

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+   return print = 'This one is on me!';
+  }
+  else if (ride > 2000 && ride < 2499) {
+    return print = 'I will gladly take your thirty bucks.';
+  }
+  else if (ride > 2500) {
+    return print = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return print = 'Ok, sounds good.';
+  }
+  else {
+    return print = 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return print = 'Thank you so much.'
+  }
+  else if (tip === 'not as generous') {
+    return print = 'Thank you.'
+  }
+  else {
+    return print = 'Bye.'
+  }
 }
